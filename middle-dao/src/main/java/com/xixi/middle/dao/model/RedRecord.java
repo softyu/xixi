666db9ae38afd57b729af271b8823a6039cce5cf
yuxi <674440302@qq.com>
2020-11-19 10:14:03 +0800

    构建抢红包基本结构

---
+++ middle-dao/src/main/java/com/xixi/middle/dao/model/RedRecord.java
@@ -0,0 +1,79 @@
+package com.xixi.middle.dao.model;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.Date;
+
+public class RedRecord implements Serializable {
+    private Long id;
+
+    private Long userId;
+
+    private String redPacket;
+
+    private Integer total;
+
+    private BigDecimal amount;
+
+    private Integer isActive;
+
+    private Date createTime;
+
+    private static final long serialVersionUID = 1L;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public String getRedPacket() {
+        return redPacket;
+    }
+
+    public void setRedPacket(String redPacket) {
+        this.redPacket = redPacket == null ? null : redPacket.trim();
+    }
+
+    public Integer getTotal() {
+        return total;
+    }
+
+    public void setTotal(Integer total) {
+        this.total = total;
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public void setAmount(BigDecimal amount) {
+        this.amount = amount;
+    }
+
+    public Integer getIsActive() {
+        return isActive;
+    }
+
+    public void setIsActive(Integer isActive) {
+        this.isActive = isActive;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+}
